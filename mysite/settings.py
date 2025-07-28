24181d9bd46f2320cee3fb88a95ba2f1ebb3aab0
Borzu Shayan <BorzuShayan@gmail.com>
2025-07-29 00:54:36 +0330

    Umm.. Hi?

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "statics",
+    BASE_DIR / "static",
 ]
 
 

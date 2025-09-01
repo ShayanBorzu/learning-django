f34564f8cb7b92a59ddaae73568c55fb5278bb99
Borzu Shayan <BorzuShayan@gmail.com>
2025-09-01 10:31:20 +0330

    himanize and site set up

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,10 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
+    "django.contrib.sites",
+    "django.contrib.humanize",
     "websiteApp.apps.WebsiteappConfig",
     "blog.apps.BlogConfig",
     
 ]
+
+SITE_ID = 2
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
